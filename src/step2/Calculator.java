52c22419284ed59d8cfb41a3b1da513d3dadfc6a
Sehyun_Kim <kimsparadise0202@gmail.com>
2025-12-09 00:33:59 +0900

    기본 기능 구현, getter setter  위치 미확정

--- src/step2/Calculator.java
+++ src/step2/Calculator.java
@@ -3,9 +3,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
-    int num1, num2;
-    char op;
-    ArrayList<Integer> results;//연산 결과를 저장하는 ArrayList
+    private int result;
+    private ArrayList<Integer> results;//연산 결과를 저장하는 ArrayList
 
     //생성자
     Calculator() {
@@ -13,55 +12,35 @@ public class Calculator {
     }
 
     //1. 0 이상의 정수 2개와 부호 1개를 받아 연산 결과를 출력하는 메서드
-    public void calculate() {
+    public int calculate(int num1, int num2, char op) {
         // 2개의 양의 정수와 부호 입력받기
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("[1/2] 0 또는 양의 정수를 입력하세요 : ");
-            num1 = scanner.nextInt();
-            System.out.print("[2/2] 0 또는 양의 정수를 입력하세요 : ");
-            num2 = scanner.nextInt();
-            System.out.print("연산 부호를 입력하세요 (+, -, *, / 중 택1) : ");
-            op =  scanner.next().charAt(0);
-            try {
-                int result;
-                switch (op) {
-                    case '+':
-                        result = num1 + num2;
-                        System.out.println(num1 + " + " + num2 + " = " + result);
-                        results.add(result);
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        System.out.println(num1 + " - " + num2 + " = " + result);
-                        results.add(result);
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        System.out.println(num1 + " * " + num2 + " = " + result);
-                        results.add(result);
-                        break;
-                    case '/':
-                        if (num2 == 0) throw new ArithmeticException();
-                        result = num1 / num2;
-                        System.out.println(num1 + " / " + num2 + " = " + result);
-                        results.add(result);
-                        break;
-                    default:
-                        System.out.println("입력이 잘못되었습니다. 연산을 수행할 수 없습니다.");
-                        break;
-                }
-            } catch (ArithmeticException e) {
-                System.out.println("0으로 나눌 수 없습니다.");
+        try {
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
+                    results.add(result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    results.add(result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    results.add(result);
+                    break;
+                case '/':
+                    if (num2 == 0) throw new ArithmeticException();
+                    result = num1 / num2;
+                    results.add(result);
+                    break;
+                default:
+                    System.out.println("입력이 잘못되었습니다. 연산을 수행할 수 없습니다.");
+                    break;
             }
-            System.out.print("아무 키나 누른 뒤 Enter를 눌러 계속하세요 \n (종료 : exit 입력) : ");
-            Scanner sc = new Scanner(System.in);
-            String response = sc.next();
-            if (response.equals("exit")) {
-                System.out.println("계산기가 종료되었습니다.");
-                break;
-            }
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다.");
         }
+        return result;
     }
 
     public void getItem(int index){
@@ -87,5 +66,13 @@ public class Calculator {
         } catch (IndexOutOfBoundsException e) {
             System.out.println("저장된 연산 결과가 없습니다.");
         }
+    }
+
+    public void setItem(int index, int new_value){
+        results.set(index, new_value);
+    }
+
+    public void deleteFirstItem(){
+        results.remove(0);
     }
 }
--- src/step2/Main.java
+++ src/step2/Main.java
@@ -1,11 +1,26 @@
 package step2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("계산기 시작");
-        Calculator calc1 = new Calculator();
-        calc1.calculate();
-        calc1.getItem(0); //단일 계산결과 조회
-        calc1.getItem(); //저장된 모든 계산결과 조회
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.print("[1/2] 음이 아닌 정수를 입력하세요 : ");
+            int num1 = scanner.nextInt();
+            System.out.print("[2/2] 음이 아닌 정수를 입력하세요 : ");
+            int num2 = scanner.nextInt();
+            System.out.print("연산 부호를 입력하세요 (+, -, *, / 중 택1) : ");
+            char op =  scanner.next().charAt(0);
+            Calculator calc1 = new Calculator();
+            int result1 = calc1.calculate(num1, num2, op);
+            System.out.println("연산 결과 : " + result1);
+            System.out.println("계속하겠습니까? : 종료는 'exit' 입력");
+            String answer = scanner.next();
+            if(answer.equals("exit")){
+                break;
+            }
+        }
     }
 }
