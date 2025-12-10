d34f42700129fa9186b3cd5efc6bacc4eb3317e0
Sehyun_Kim <kimsparadise0202@gmail.com>
2025-12-10 18:07:48 +0900

    calculate()가 double 타입을 항상 반환하는 상태

--- src/step3/Calculator.java
+++ src/step3/Calculator.java
@@ -16,32 +16,39 @@ public class Calculator<T extends Number> {
     //1. 0 이상의 정수 2개와 부호 1개를 받아 연산 결과를 출력하는 메서드
     //calculate는 String을 바로 받는 것이 아니라 Operator.testSymbol() 메서드를 거치고 필터링된 enum 타입을 전달받음
     public double calculate(T num1, T num2, Operators op) throws  InputMismatchException {
-
-        //Number 타입임을 활용, 계산 가능한 형태로 변경
-        double value1 = num1.doubleValue();
-        double value2 = num2.doubleValue();
-        double result;
-        switch (op) {
-            case PLUS:
-                result = value1 + value2;
-                results.add(result);
-                return result;
-            case MINUS:
-                result = value1 - value2;
-                results.add(result);
-                return result;
-            case MULTIPLY:
-                result = value1 * value2;
-                results.add(result);
-                return result;
-            case DIVIDE:
-                if (value2 == 0.0)
-                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
-                result = value1 / value2;
-                results.add(result);
-                return result;
-            default:
-                throw new IllegalStateException("알 수 없는 연산자입니다");
+        try {
+            //Number 타입임을 활용, 계산 가능한 형태로 변경
+            double value1 = num1.doubleValue();
+            double value2 = num2.doubleValue();
+            double result;
+            switch (op) {
+                case PLUS:
+                    result = value1 + value2;
+                    results.add(result);
+                    return result;
+                case MINUS:
+                    result = value1 - value2;
+                    results.add(result);
+                    return result;
+                case MULTIPLY:
+                    result = value1 * value2;
+                    results.add(result);
+                    return result;
+                case DIVIDE:
+                    if (value2 == 0.0)
+                        throw new ArithmeticException();
+                    result = value1 / value2;
+                    results.add(result);
+                    return result;
+                default:
+                    throw new IllegalStateException();
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("0으로 나눌 수 없습니다.");
+            return 0;
+        } catch (IllegalArgumentException e) {
+            System.out.println("알 수 없는 연산자입니다");
+            return 0;
         }
     }
 
